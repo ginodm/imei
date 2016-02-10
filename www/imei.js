dd4b9d5e3f25e25ed2accbf266ad4fe81cc5067b
ginodm <ginode.maria@gmail.com>
2016-02-10 14:09:30 +0100

    pp

--- www/imei.js
+++ www/imei.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var imeiExport = {};
 
 imeiExport.getImei = function (successCallback, errorCallback){
-		exec(successCallback, errorCallback, "IMEI", "get", []);
+		exec(successCallback, errorCallback, "imei", "get", []);
     };
 	
 module.exports = imeiExport;
